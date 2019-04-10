b96cbdf43930307ee1afa9a43886f5392ce15b72
mindpixel-labs <anthonypinskeyjpn@gmail.com>
2019-04-09 23:38:22 -0400

    Add CategoryHeading parameters model

--- src/models/parameters.ts
+++ src/models/parameters.ts
@@ -3,13 +3,17 @@ import Categories from './parameters/Categories';
 import Info from './parameters/Info';
 import PrevEntry from './parameters/PrevEntry';
 import NextEntry from './parameters/NextEntry';
+import CategoryArchive from './parameters/CategoryArchive';
+import CategoryHeading from './parameters/CategoryHeading';
 
 const Completions: Array<any> = [
   Channel,
   Categories,
   Info,
   PrevEntry,
-  NextEntry
+  NextEntry,
+  CategoryArchive,
+  CategoryHeading
 ];
 
 export default Completions;
---
+++ src/models/parameters/CategoryHeading.ts
@@ -0,0 +1,12 @@
+import CompletionItem from '../../interfaces/completion.interface';
+
+const CategoryHeading: CompletionItem = {
+  prefix: 'exp:channel:category_heading',
+  items: [
+    'disable',
+    'relaxed_categories',
+    'channel'
+  ]
+};
+
+export default CategoryHeading;
